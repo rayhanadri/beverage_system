140ca0c7d14c174aeef7a5ba71a6c39a81b1588d
rayhanadri <rayerzz99@gmail.com>
2025-03-22 20:58:32 +0700

    revise connection

--- app/config/sql.go
+++ app/config/sql.go
@@ -3,15 +3,30 @@ package config
 import (
 	"database/sql"
 	"fmt"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
 	var err error
-	db, err := sql.Open("mysql", "root:@/RMT9_P1")
+
+	err = godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Println(err)
 	}
